a55cf8326c3ea45034a697f2729c700df12eebe4
WONGMANCHUN_MAC <hwj2661065>
2018-03-07 17:36:37 +0800

    添加地图

---
+++ src/assets/MapMain.js
@@ -0,0 +1,231 @@
+/**
+ * Created by WONGMANCHUN_MAC on 2017/12/12.
+ */
+var parks = [{
+  "name": "P1停车场",
+  "center": "113.404019,23.174349",
+  "type": 0,
+  "subDistricts": []
+},
+  {
+    "name": "P2停车场",
+    "center": "113.400843,23.174788",
+    "type": 1,
+    "subDistricts": []
+  },
+  {
+    "name": "P3停车场",
+    "center": "113.399137,23.173545",
+    "type": 2,
+    "subDistricts": []
+  },
+  {
+    "name": "P4停车场",
+    "center": "113.400188,23.175779",
+    "type": 3,
+    "subDistricts": []
+  },
+  {
+    "name": "P5停车场",
+    "center": "113.401154,23.177002",
+    "type": 4,
+    "subDistricts": []
+  }
+  ,
+  {
+    "name": "P6停车场",
+    "center": "113.398558,23.176163",
+    "type": 5,
+    "subDistricts": []
+  },
+  // {
+  //   "name": "P7停车场",
+  //   "center": "113.398322,23.175005",
+  //   "type": 4,
+  //   "subDistricts": []
+  // }
+  // ,
+  {
+    "name": "P8停车场",
+    "center": "113.403241,23.173372",
+    "type": 6,
+    "subDistricts": []
+  }
+];
+
+var buildings = [
+  {
+  "name": "研发楼A楼",
+  "center": "113.403488,23.175261",
+  "type": 0,
+  "subDistricts": []
+  },
+  {
+    "name": "研发楼B楼",
+    "center": "113.403096,23.175498",
+    "type": 1,
+    "subDistricts": []
+  },
+  {
+    "name": "研发楼C楼",
+    "center": "113.403064,23.175922",
+    "type": 2,
+    "subDistricts": []
+  },
+  {
+    "name": "研发楼D楼",
+    "center": "113.402817,23.176297",
+    "type": 3,
+    "subDistricts": []
+  },
+  {
+    "name": "研发楼E楼",
+    "center": "113.402431,23.176533",
+    "type": 4,
+    "subDistricts": []
+  },
+  {
+    "name": "创新中心A座 1.1",
+    "center": "113.400339,23.175231",
+    "type": 5,
+    "subDistricts": []
+  },
+  {
+    "name": "创新中心A座 1.2",
+    "center": "113.400167,23.1746",
+    "type": 6,
+    "subDistricts": []
+  },
+  {
+    "name": "创新中心A座 1.3",
+    "center": "113.400135,23.173939",
+    "type": 7,
+    "subDistricts": []
+  },
+  {
+    "name": "数据中心A座 2.2",
+    "center": "113.3994,23.175517",
+    "type": 8,
+    "subDistricts": []
+  },
+  {
+    "name": "数据中心B座 2.3",
+    "center": "113.399255,23.174719",
+    "type": 9,
+    "subDistricts": []
+  },
+  {
+    "name": "数据中心C座 3.1",
+    "center": "113.399073,23.173949",
+    "type": 10,
+    "subDistricts": []
+  },
+  {
+    "name": "服务中心A座",
+    "center": "113.399952,23.176198",
+    "type": 11,
+    "subDistricts": []
+  },
+  {
+    "name": "服务中心B座",
+    "center": "113.399437,23.176484",
+    "type": 12,
+    "subDistricts": []
+  },
+  {
+    "name": "合作交流中心A座",
+    "center": "113.402871,23.178851",
+    "type": 13,
+    "subDistricts": []
+  },
+  {
+    "name": "合作交流中心B座",
+    "center": "113.402163,23.179206",
+    "type": 14,
+    "subDistricts": []
+  },
+  {
+    "name": "合作交流中心C座",
+    "center": "113.401379,23.179029",
+    "type": 15,
+    "subDistricts": []
+  },
+  {
+    "name": "合作交流中心D座",
+    "center": "113.400478,23.178476",
+    "type": 16,
+    "subDistricts": []
+  },
+  {
+    "name": "合作交流中心E座",
+    "center": "113.400585,23.178013",
+    "type": 17,
+    "subDistricts": []
+  },
+  {
+    "name": "监控展示中心2.1",
+    "center": "113.400902,23.177401",
+    "type": 18,
+    "subDistricts": []
+  },
+  {
+    "name": "变电站",
+    "center": "113.398166,23.176297",
+    "type": 19,
+    "subDistricts": []
+  },
+  {
+    "name": "足球场",
+    "center": "113.401256,23.173969",
+    "type": 20,
+    "subDistricts": []
+  },
+  {
+    "name": "日月湖",
+    "center": "113.403767,23.176755",
+    "type": 21,
+    "subDistricts": []
+  },
+  {
+    "name": "智慧桥",
+    "center": "113.402683,23.177604",
+    "type": 22,
+    "subDistricts": []
+  },
+  {
+    "name": "灵秀山",
+    "center": "113.402259,23.17501",
+    "type": 23,
+    "subDistricts": []
+  },
+  {
+    "name": "环山路1",
+    "center": "113.401733,23.174181",
+    "type": 24,
+    "subDistricts": []
+  },
+  {
+    "name": "环山路2",
+    "center": "113.403514,23.175917",
+    "type": 25,
+    "subDistricts": []
+  },
+  {
+    "name": "智慧路",
+    "center": "113.400124,23.17567",
+    "type": 26,
+    "subDistricts": []
+  },
+  {
+    "name": "创新路",
+    "center": "113.400779,23.17427",
+    "type": 27,
+    "subDistricts": []
+  },
+  {
+    "name": "大门",
+    "center": "113.40404,23.173663",
+    "type": 28,
+    "subDistricts": []
+  }
+];
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+declare var AMap: any;
+declare var parks: any;
+declare var buildings: any;
 
 /**
  * Generated class for the MapPage page.
@@ -14,12 +17,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'map.html',
 })
 export class MapPage {
-
+  isMask: boolean = true;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
+    this.mapInit();
+  }
+
+  mapInit(){
+    //初始化加载地图
+    let map = new AMap.Map('container', {
+      mapStyle: 'amap://styles/4baab3de6130840ec99a93e129518818',//样式URL
+      resizeEnable: true,
+      dragEnable: true,
+      keyboardEnable: true,
+      doubleClickZoom: true,
+      zoom: 16,
+      zooms:[16,18],
+      center: [113.401572,23.175374]
+    });
+
+    //限制可视区域
+    setLimitBounds();
+    function setLimitBounds() {
+      map.setLimitBounds(map.getBounds());
+    }
+
+    //设置地图显示内容
+    map.setFeatures(['road', 'point', 'building', 'bg']);
+
+    //增加工具条
+    AMap.plugin(['AMap.ToolBar', 'AMap.Scale', 'AMap.MapType'],
+      function () {
+        map.addControl(new AMap.ToolBar());//加载工具条(ToolBar){ locationMarker: customMarker }
+        map.addControl(new AMap.Scale());//加载比例尺
+        // map.addControl(new AMap.MapType());//加载地图实景（含路况）
+      });
+
+
   }
 
 }
